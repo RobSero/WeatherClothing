a6530a93fbc84194ee589c8a73bbf98f613d711f
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-25 18:46:48 +0000

    deploy

--- config/environment.js
+++ config/environment.js
@@ -1,1 +1,3 @@
-export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/clothingDB'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/clothingDB'
+
+module.exports = dbURI
